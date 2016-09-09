b73d677676e1696c90e4b1c85b080cb5f4e27ccb
Wuttipat <wuttipat@outlook.com>
2016-09-09 23:14:05 +0700

    Setup web api

--- server.js
+++ server.js
@@ -3,6 +3,15 @@ var app=express();
 app.set('views',__dirname);
 app.set('view engine','jade');
 app.use(express.static(__dirname));
+
+// Set up web api
+app.get('/api/users',function(req,res){
+	res.send([
+		{firstname:"John",lastname:"Travota"},
+		{firstname:"Jame",lastname:"Mars"}
+	]);
+});
+
 app.get('*',function(req,res){
 	res.render('index');
 });
